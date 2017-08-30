859b1f3e390f89d5913e4245840075d65cb7db99
dhiran khanal <jungrupak@gmail.com>
2017-08-30 21:01:19 +0545

    created project with all files

---
+++ source/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    //Responsive Nav
+    $('.jr-custom-nav .jr-nav-trigger').on('click', function () {
+
+        $(this).siblings('ul').toggleClass('show-nav');
+        $(this).toggleClass('close-nav');
+
+    });
+
+
+
+
+})//Ends==========
